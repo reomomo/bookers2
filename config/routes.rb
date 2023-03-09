23407e17fe45c174248f61d52af89097148f6e4b
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-03-09 10:22:50 +0000

    [Add]booksのviewのリンク等

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books
-  resources :users, only: [:show, :edit, :index]
+  resources :users, only: [:show, :edit, :update, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
